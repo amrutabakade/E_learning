c835de82ccd3d9ca5bd33c2281198b15f12dacec
AMRUTA BAKADE <amrutabakade@gmail.com>
2024-06-26 16:35:53 +0530

    Your commit message here

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,11 @@
 import TryCatch from "../middlewares/TryCatch.js";
 import { Courses } from "../models/Courses.js";
 import { Lecture } from "../models/Lecture.js";
+import {rm} from "fs"; // to delete videos,files uploaded on {upload folder}
+import { promisify } from "util";
+import fs from 'fs'
+import { User } from "../models/User.js";
+
 
 export const createCourse = TryCatch(async (req, res) => {
     const { title, description, category, createdBy, duration, price } = req.body;
@@ -47,4 +52,60 @@ export const createCourse = TryCatch(async (req, res) => {
       message: "lecture added",
       lecture,
     })
-  })
+  });
+
+
+  //only admin can delete lectures
+
+  export const deleteLecture = TryCatch(async(req, res)=>
+  {
+    const lecture = await Lecture.findById(req.params.id);
+
+    rm(lecture.video,()=>
+    {
+      console.log("vido deleted");
+    })
+
+    await lecture.deleteOne();
+    res.json({ message: "Lecture Deleted"});
+  });
+
+  const unlikeAsync = promisify(fs.unlink);
+
+  export const deleteCourse = TryCatch(async(req, res)=>
+    {
+      //find course into Courses db
+      const course = await Courses.findById(req.params.id);
+      
+      // find all lectures of that course to delete
+      const  lectures = await Lecture.find({course: course._id});
+      
+      //first remove all videos of lecture
+      await Promise.all(
+        lectures.map(async(lecture)=>{
+          await unlikeAsync(lecture.video);
+          console.log("video deleted");
+        })
+      );
+      // them delete course image
+      rm(course.image, ()=>{
+        console.log("image deleted");
+      })
+
+      // then delete all lectures
+      await Lecture.find({course: req.params.id}).deleteMany();
+
+      //then delete course
+      await course.deleteOne();
+
+      //remove the course from users subscription
+      await User.updateMany({},{$pull:{subscription : req.params.id}});
+
+      res.json(
+        {
+          message: "Course Deleted",
+        }
+      )
+    });
+
+  
--- controllers/course.js
+++ controllers/course.js
@@ -1,5 +1,7 @@
 import TryCatch from "../middlewares/TryCatch.js";
 import {Courses} from '../models/Courses.js';
+import {Lecture} from '../models/Lecture.js'
+import {User} from '../models/User.js'
 
 export const getAllCourses = TryCatch(async(req,res)=>{
     const courses = await Courses.find();
@@ -14,4 +16,50 @@ export const getSingleCourse = TryCatch(async(req,res)=>{
     res.json({
         course,
     })
-})
+});
+
+//fetch all lec
+export const fetchLectures = TryCatch(async(req,res)=>
+{
+    const lectures = await Lecture.find({course: req.params.id});
+
+    const user = await User.findById(req.user._id)
+
+    //if user is admin he will get accesss to lectures
+    if(user.role === "admin")
+    {
+        return res.json({lectures});
+    }
+
+    // if user is not admin he has to subscribe
+    if(!User.subscription.includes(req.params.id)) 
+    return res.status(400).json(
+    {
+        message : "you have not subscribed to this corse",
+    });
+
+    res.json({lectures});
+});
+
+//fetch single lec
+export const fetchLecture = TryCatch(async(req,res)=>
+    {
+        const lecture = await Lecture.findById(req.params.id);
+    
+        const user = await User.findById(req.user._id)
+    
+        //if user is admin he will get accesss to lectures
+        if(user.role === "admin")
+        {
+            return res.json({lecture});
+        }
+    
+        // if user is not admin he has to subscribe
+        if(!User.subscription.includes(req.params.id)) 
+        return res.status(400).json(
+        {
+            message : "you have not subscribed to this corse",
+        });
+    
+        res.json({lecture});
+    })
